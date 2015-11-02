df0feedfaf5be6a57a51afc9f64b9a5b6384346b
bkrell <bkrell@gmail.com>
2015-11-01 17:30:53 -0800

    clean up recipes and set up test kitchen

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,4 @@
 include_recipe 'selinux::permissive'
-include_recipe 'mongotest::db'
-include_recipe 'mongotest::firewall'
+#include_recipe 'mongotest::db'
+include_recipe "mongodb::default"
+#include_recipe 'mongotest::firewall'
